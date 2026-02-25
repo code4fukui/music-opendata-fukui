14ce0e4a109c44f35e630f5aa45f9b0b26405cbd
Taisuke Fukuno <fukuno@jig.jp>
2026-02-25 23:16:56 +0900

    fix for playlist data

--- makePlaylist.js
+++ makePlaylist.js
@@ -4,17 +4,13 @@ export const makePlaylist = async (playlistid) => {
   const url = "https://suno.com/playlist/" + playlistid;
 
   const html = await fetchOrLoad(url);
-  let idx = `self.__next_f.push([1,"5:[\\"$\\",\\"$L1b\\",null,{\\"playlist\\"`;
-  let idxoff = 2;
-  let n = html.indexOf(idx);
-  console.log(n);
-  if (n < 0) {
-    idx = `self.__next_f.push([1,"12:[\\"$\\",\\"$L23\\",null,{\\"playlist\\":`
-    n = html.indexOf(idx);
-    idxoff = 3;
-  console.log(n);
-    if (n < 0) throw new Error("not found playlist on HTML");
-  }
+  const re = /self\.__next_f\.push\(\[1,"(\d+):\[\\"\$\\",\\"\$L[0-9a-fA-F]+\\",null,\{\\"playlist\\"/;
+  const mm = html.match(re);
+  if (!mm) throw new Error("not fonud playlist on HTML");
+  const idx = mm[0];
+  const offidx = mm[1].length;
+  const n = mm.index;
+  console.log(n, idx);
   // "]}}]\n"])</script><script>self.__next_f.push([1,"12:{\"met
   const indexOfEnd = (s, i) => {
     let state = 0;
@@ -37,12 +33,10 @@ export const makePlaylist = async (playlistid) => {
   };
 
   const m = indexOfEnd(html, n + idx.length);
-  console.log(m);
-  const s = unescape(html.substring(n + "self.__next_f.push([1,\"".length + idxoff, m - 2));
+  const s = unescape(html.substring(n + "self.__next_f.push([1,\"".length + 1 + offidx, m - 2));
   //console.log(s);
   //await Deno.writeTextFile("playlist.json", s);
   const json = JSON.parse(s);
-  console.log(json);
 
   const playlist = json[3].playlist;
   await Deno.writeTextFile("playlist_org.json", JSON.stringify(playlist, null, 2));
